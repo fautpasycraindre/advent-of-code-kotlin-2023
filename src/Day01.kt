8fbdb3b743bb8bd74c8d9eb3b909fe23a1e4bd28
DamienG <granierdamien@gmail.com>
2023-12-09 15:24:18 +0100

    Day01 - part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var counter = 0
+        val digits = input.map { it.filter { it.isDigit() } }
+        digits.println()
+        for (digit in digits) {
+            if (digit.length == 2)
+                counter += digit.toInt()
+            if (digit.length > 2)
+                counter += (digit[0].toString() + digit[digit.length - 1].toString()).toInt()
+            if (digit.length == 1)
+                counter += (digit + digit).toInt()
+        }
+        counter.println()
+        return counter
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +21,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+
 
     val input = readInput("Day01")
     part1(input).println()
